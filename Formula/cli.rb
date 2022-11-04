896b194c2b5870c637ab50c06b22ecc0450d48d1
Ben LeBlond <ben@leblond.us>
2022-11-04 07:49:14 -0500

    fixing urls

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -6,13 +6,13 @@ class Cli < Formula
   version "0.0.1-beta4"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/liblaber/homebrew-liblab-cli/releases/download/#{version}/liblab-macos-amd64-cli.zip"
+    url "https://github.com/liblaber/homebrew-liblab/releases/download/#{version}/liblab-macos-amd64-cli.zip"
     sha256 "dd36986c7802d34fa98a459ab9c4f9571dfdd5748440e8d2cdb1054003af1673"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/liblaber/homebrew-liblab-cli/releases/download/#{version}/liblab-macos-arm64-cli.zip"
+    url "https://github.com/liblaber/homebrew-liblab/releases/download/#{version}/liblab-macos-arm64-cli.zip"
     sha256 "6173e09a9c8778f692d720f0e1e8df6e2724dbad9101cca87d7f3f541037185c"
   elsif OS.linux?
-    url "https://github.com/liblaber/homebrew-liblab-cli/releases/download/#{version}/liblab-linux-x64-cli.zip"
+    url "https://github.com/liblaber/homebrew-liblab/releases/download/#{version}/liblab-linux-x64-cli.zip"
     sha256 "b75633dc94f1e999a8612c58bd15fd082ab979e8b3ec5c439f8157c2fee25cfc"
   end
 
