d15bc4598a1357cfcde680673023578157f65c5b
Ben LeBlond <ben@leblond.us>
2022-11-04 07:46:49 -0500

    release 0.0.1-beta4

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,19 +1,19 @@
 require 'formula'
 
-class Cli < Formula
+class Liblab < Formula
   desc "Liblab CLI - A CLI for generating SDKs (and docs) from API specs"
   homepage "https://liblab.com"
-  version "0.0.1-beta3"
+  version "0.0.1-beta4"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/liblaber/homebrew-liblab/releases/download/0.0.1-beta3/liblab-macos-amd64-cli.zip"
-    sha256 "cbf0c9074b87cadc65f8bc66fb6e655f423c036cb9c81eb37b00837a05689db2"
+    url "https://github.com/liblaber/homebrew-liblab-cli/releases/download/#{version}/liblab-macos-amd64-cli.zip"
+    sha256 "dd36986c7802d34fa98a459ab9c4f9571dfdd5748440e8d2cdb1054003af1673"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/liblaber/homebrew-liblab/releases/download/0.0.1-beta3/liblab-macos-arm64-cli.zip"
-    sha256 "75b1fb913f1a3815b787fed56dfe91ab1734d26f63028985f302b44deac4fa22"
+    url "https://github.com/liblaber/homebrew-liblab-cli/releases/download/#{version}/liblab-macos-arm64-cli.zip"
+    sha256 "6173e09a9c8778f692d720f0e1e8df6e2724dbad9101cca87d7f3f541037185c"
   elsif OS.linux?
-    url "https://github.com/liblaber/homebrew-liblab/releases/download/0.0.1-beta3/liblab-linux-x64-cli.zip"
-    sha256 "fb968ace4afb5623e6ffc76ec415c218b7f5d6230f3a8e9764516aff72dfed3f"
+    url "https://github.com/liblaber/homebrew-liblab-cli/releases/download/#{version}/liblab-linux-x64-cli.zip"
+    sha256 "b75633dc94f1e999a8612c58bd15fd082ab979e8b3ec5c439f8157c2fee25cfc"
   end
 
   def install
@@ -26,26 +26,11 @@ class Cli < Formula
       bin.install_symlink libexec/"liblab_macos_arm64" => "liblab"
     elsif OS.linux?
       chmod(0755, "#{libexec}/linux-x64/liblab_linux_amd64")
-      bin.install_symlink libexec/linux-x64/"liblab_linux_amd64" => "liblab"      
+      bin.install_symlink libexec/linux-x64/"liblab_linux_amd64" => "liblab"
     end
   end
 
   test do
-    assert_equal "Liblab CLI - A CLI for generating SDKs (and docs) from API specs
-
-Usage: liblab <command> [arguments]
-
-Global options:
--h, --help    Print this usage information.
-
-Available commands:
-  forgot_password   Initiates a password reset
-  gen:docs          Generate docs for a given OpenAPI spec
-  gen:sdk           Generate an SDK for a given OpenAPI spec
-  login             Login to Liblab
-  logout            Logout - Sign Out from Liblab
-  register          Register a new user
-
-Run \"liblab help <command>\" for more information about a command.", shell_output("#{bin}/liblab")
+    assert_equal "liblab-cli v0.0.1-beta", shell_output("#{bin}/liblab version")
   end
 end
