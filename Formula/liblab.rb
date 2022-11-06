c04e0af80dbcd079d2d9610a8b32327b2bcb1f4d
Ben LeBlond <ben@leblond.us>
2022-11-06 17:36:09 -0600

    Update liblab.rb

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -28,7 +28,7 @@ class Liblab < Formula
       bin.install_symlink libexec/"liblab_macos_arm64" => "liblab"
     elsif OS.linux?
       chmod(0755, "#{libexec}/liblab_linux_amd64")
-      bin.install_symlink "libexec/liblab_linux_amd64" => "liblab"
+      bin.install_symlink libexec/"liblab_linux_amd64" => "liblab"
     end
   end
 
