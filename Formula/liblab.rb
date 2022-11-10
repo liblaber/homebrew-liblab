1a68a246602246f7ac3aa4501a32949b702c1045
Ben LeBlond <ben@leblond.us>
2022-11-10 06:59:16 -0600

    Bumping to 0.0.1-beta6

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -3,17 +3,17 @@ require 'formula'
 class Liblab < Formula
   desc "Liblab CLI - A CLI for generating SDKs (and docs) from API specs"
   homepage "https://liblab.com"
-  version "0.0.1-beta5"
+  version "0.0.1-beta6"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/liblaber/homebrew-liblab/releases/download/#{version}/liblab-macos-amd64-cli.zip"
-    sha256 "8ace28ac865672498e78f31275671edb423a70238b6aeb5d8cabe2638d89fdbe"
+    sha256 "37014fef4b9206b6b95bfdca63fde0f3bcae455c5f752a670ca1e00ed06399c8"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/liblaber/homebrew-liblab/releases/download/#{version}/liblab-macos-arm64-cli.zip"
-    sha256 "59cb877567ee9c987c652b93f973f95fb751baa6ce6dcc8822fbea2429ad0ef7"
+    sha256 "25d04906cdff7900e40082773658f72075975c0439aab32a092dac049d6b9fa6"
   elsif OS.linux?
     url "https://github.com/liblaber/homebrew-liblab/releases/download/#{version}/liblab-linux-x64-cli.zip"
-    sha256 "674113911332b471d3029059fa92be7325002984ca5837764a2b435a7a94c979"
+    sha256 "741c2d37cff50d5d45a0ef29ca2cfd78e0a75584ed64d80571f4fe3d60293246"
   end
 
   def install
