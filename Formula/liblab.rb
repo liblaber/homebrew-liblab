dad259b18bae94425e6019ec2f4025e523353631
Cameron Steele <cam.steeleis@gmail.com>
2024-01-22 14:05:47 -0600

    Test commit to GH

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -4,24 +4,23 @@ class Liblab < Formula
   homepage "https://liblab.com"
   version "0.0.0"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/8245273/liblab-v0.35.1-8245273-darwin-x64.tar.gz"
-    sha256 "eb8cde94b9029e204692ba42a6cfa2c00b378daa4129cc82455f0000a02eb7f0"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/72eb6ed/liblab-v0.35.1-72eb6ed-darwin-x64.tar.gz"
+    sha256 "3a0ea2e77cff4690fd520586984b525f7af131c9ecdc898146f001946c9fc39d"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/8245273/liblab-v0.35.1-8245273-darwin-arm64.tar.gz"
-    sha256 "68cfe87db7ad009e813711991cae547b62905bacc08c8566e2efb0e698e7642a"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/72eb6ed/liblab-v0.35.1-72eb6ed-darwin-arm64.tar.gz"
+    sha256 "89d52c5d36e142a434d65013ae33c13d6d990adfe6d32ebf03bd040b0ca21715"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/8245273/liblab-v0.35.1-8245273-linux-arm64.tar.gz"
-    sha256 "38974a9f39f279386dc86bfa63dea4541ec2a959cede8b863f95c8d78a5d9d55"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/72eb6ed/liblab-v0.35.1-72eb6ed-linux-arm64.tar.gz"
+    sha256 "0715a08715531e458af26dd59576a51f7252ca0ab07512a016166202fc9efb56"
   elsif OS.linux?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/8245273/liblab-v0.35.1-8245273-linux-x64.tar.gz"
-    sha256 "04134398dde39051f8d043f3682756eebf52c9ba35d8539af42c6fad624fd5b7"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/72eb6ed/liblab-v0.35.1-72eb6ed-linux-x64.tar.gz"
+    sha256 "4f95909ddd91d950bd7afcd1f71cd6722812066bee50ecff175edc722863590d"
   end
   depends_on "node"
-
   def install
-    inreplace "bin/liblab", /^CLIENT_HOME=/, "export LibLab_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/liblab", /^CLIENT_HOME=/, "export LibLab_OCLIF_CLIENT_HOME=#{lib/"client"}
+CLIENT_HOME="
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/liblab"
   end
-
 end
