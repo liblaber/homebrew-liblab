93f91af8619a980a3e36a6457d1af658ecebfe00
Cameron Steele <cam.steeleis@gmail.com>
2024-01-29 16:16:54 -0600

    Test commit to GH

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -2,26 +2,25 @@ require "language/node"
 class Liblab < Formula
   desc "liblab CLI - A CLI for generating SDKs (and docs) from API specs"
   homepage "https://liblab.com"
-  version "0.0.0"
+  version "0.36.0"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/8245273/liblab-v0.35.1-8245273-darwin-x64.tar.gz"
-    sha256 "eb8cde94b9029e204692ba42a6cfa2c00b378daa4129cc82455f0000a02eb7f0"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.36.0/57cff0b/liblab-v0.36.0-57cff0b-darwin-x64.tar.gz"
+    sha256 "8e9fb87077d04cb6f78b69c0ddfed06200eafd11dc093734fd0cf137751bc308"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/8245273/liblab-v0.35.1-8245273-darwin-arm64.tar.gz"
-    sha256 "68cfe87db7ad009e813711991cae547b62905bacc08c8566e2efb0e698e7642a"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.36.0/57cff0b/liblab-v0.36.0-57cff0b-darwin-arm64.tar.gz"
+    sha256 "70f548f3556ddbee7afed77efd6f1b6db2f13549baff3ce516f19ed0b8f36ead"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/8245273/liblab-v0.35.1-8245273-linux-arm64.tar.gz"
-    sha256 "38974a9f39f279386dc86bfa63dea4541ec2a959cede8b863f95c8d78a5d9d55"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.36.0/57cff0b/liblab-v0.36.0-57cff0b-linux-arm64.tar.gz"
+    sha256 "ebe7c8bbdf9b7161d060ed64d4612c412884a0a4e9827379ffdef1b23aa07c19"
   elsif OS.linux?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.35.1/8245273/liblab-v0.35.1-8245273-linux-x64.tar.gz"
-    sha256 "04134398dde39051f8d043f3682756eebf52c9ba35d8539af42c6fad624fd5b7"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.36.0/57cff0b/liblab-v0.36.0-57cff0b-linux-x64.tar.gz"
+    sha256 "712941984cac1aef66f808e283b3cf33424fba2e10f46ea87bf39f0f543b1f17"
   end
   depends_on "node"
-
   def install
-    inreplace "bin/liblab", /^CLIENT_HOME=/, "export LibLab_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
+    inreplace "bin/liblab", /^CLIENT_HOME=/, "export LibLab_OCLIF_CLIENT_HOME=#{lib/"client"}
+CLIENT_HOME="
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/liblab"
   end
-
 end
