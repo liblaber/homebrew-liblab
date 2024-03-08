28280061e03d82ff4b97ad6602f2169b9049062d
Cameron Steele <cam.steeleis@gmail.com>
2024-03-08 08:30:53 -0600

    Test commit to GH

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -2,19 +2,19 @@ require "language/node"
 class Liblab < Formula
   desc "liblab CLI - A CLI for generating SDKs (and docs) from API specs"
   homepage "https://liblab.com"
-  version "0.38.4"
+  version "0.38.5"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.38.4/ce11b06/liblab-v0.38.4-ce11b06-darwin-x64.tar.gz"
-    sha256 "fbcbcaa345513d1472ad64503ed15d1fa6e27ee94ac474411777dce093bdb523"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.38.5/d135883/liblab-v0.38.5-d135883-darwin-x64.tar.gz"
+    sha256 "7481445ec0ddac4728d4a4922599abe0e3e2344fa150f4c2bb2b6600d2b30d0b"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.38.4/ce11b06/liblab-v0.38.4-ce11b06-darwin-arm64.tar.gz"
-    sha256 "3264e702db6f70e8a8e660e52e63d70b4bfced6de0856b0fd51516b2b160b841"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.38.5/d135883/liblab-v0.38.5-d135883-darwin-arm64.tar.gz"
+    sha256 "6faaf14a4e5aa4dd560d990d4ae0139ff4bcc85a9864dbcac2683ebe6f3dd4a1"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.38.4/ce11b06/liblab-v0.38.4-ce11b06-linux-arm64.tar.gz"
-    sha256 "7a9d7a2403afd625d09e7c70f1155f4e9a19056ff29b2c7ec6d27be3651f1471"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.38.5/d135883/liblab-v0.38.5-d135883-linux-arm64.tar.gz"
+    sha256 "27b43a32b406d228b09def1ee2a3b079c35adfec3ba9dd3ab8c05fa618e154f1"
   elsif OS.linux?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.38.4/ce11b06/liblab-v0.38.4-ce11b06-linux-x64.tar.gz"
-    sha256 "9269f211beda9c0218eb6cac59376276cde00d3dd1d958f5dcdd29b5f42ea85b"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.38.5/d135883/liblab-v0.38.5-d135883-linux-x64.tar.gz"
+    sha256 "1b9bd84948fa10377be05fb1d85622cfe2af80bdac77784da886a5f34584e4a5"
   end
   depends_on "node"
   def install
