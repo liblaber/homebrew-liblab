74fe7136f7b5fafccc6d4246b73b4b746deb18f5
Cameron Steele <cam.steeleis@gmail.com>
2024-10-23 15:57:35 -0500

    Test commit to GH

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -2,19 +2,19 @@ require "language/node"
 class Liblab < Formula
   desc "liblab CLI - A CLI for generating SDKs (and docs) from API specs"
   homepage "https://liblab.com"
-  version "0.49.1"
+  version "0.49.3"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.1/dd083b7/liblab-v0.49.1-dd083b7-darwin-x64.tar.gz"
-    sha256 "db9478c19a27bcaacee793b9ad14e7985ee404f643480ad8803f815d852c858f"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.3/32203de/liblab-v0.49.3-32203de-darwin-x64.tar.gz"
+    sha256 "c82110b80c01768fd88354179031cf92c1366669f6aeb81485e391afedad494a"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.1/dd083b7/liblab-v0.49.1-dd083b7-darwin-arm64.tar.gz"
-    sha256 "f82a9160fa4c7c59e562c35fe99f419c9ab6fd008f4da41ef37aebcecba0cc72"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.3/32203de/liblab-v0.49.3-32203de-darwin-arm64.tar.gz"
+    sha256 "38c530671eb84c10a10b3c6219da41c83e7f63e7d677365f639ba446e02369dd"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.1/dd083b7/liblab-v0.49.1-dd083b7-linux-arm.tar.gz"
-    sha256 "d5a1badbaf071565b4e2fd47ead94144161f12a930d0ce8b0b62d9a4f8e662f9"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.3/32203de/liblab-v0.49.3-32203de-linux-arm.tar.gz"
+    sha256 "c7997359fc91d78b31c66a5660d6bf81e94a9fa3be4e84a8d2e1f774b404f509"
   elsif OS.linux?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.1/dd083b7/liblab-v0.49.1-dd083b7-linux-x64.tar.gz"
-    sha256 "e52619a2180a4872a7afc8d13a9775c6f757a8561da2a2431eceeed6cc2f5ff1"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.3/32203de/liblab-v0.49.3-32203de-linux-x64.tar.gz"
+    sha256 "361c8204db4f52e767caeb711bb386fdaa7e7f38dd7ccea81753fa9cb9dcabbd"
   end
   depends_on "node"
   def install
