0a1e0509b62cdb50168f3dca3887247c14de4c13
ATechAdventurer <cam.steeleis@gmail.com>
2025-02-11 12:09:49 -0600

    Test commit to GH

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -2,19 +2,19 @@ require "language/node"
 class Liblab < Formula
   desc "liblab CLI - A CLI for generating SDKs (and docs) from API specs"
   homepage "https://liblab.com"
-  version "0.49.13"
+  version "0.49.14"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.13/c570a66/liblab-v0.49.13-c570a66-darwin-x64.tar.gz"
-    sha256 "9ba1adf341431e9da894c2c40ba6ceef55d86c0b7f970abf546125dc48313053"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.14/d3dcebf/liblab-v0.49.14-d3dcebf-darwin-x64.tar.gz"
+    sha256 "a4dae124f6159b59682f8e6f6f020cb9b90614c4fda4cb8f94e3073521374490"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.13/c570a66/liblab-v0.49.13-c570a66-darwin-arm64.tar.gz"
-    sha256 "49066b346148ea39bbb0e9a957d1d048773ecf7ee603b2963d827281e64ba30e"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.14/d3dcebf/liblab-v0.49.14-d3dcebf-darwin-arm64.tar.gz"
+    sha256 "a85bebd70501439fb41a6590a41016b199eaf9661db540c17b3107225f22a208"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.13/c570a66/liblab-v0.49.13-c570a66-linux-arm.tar.gz"
-    sha256 "43060f9ba27fd25a22ed2f3d70baec5c16047baf9af6b131ad86d5a331e86436"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.14/d3dcebf/liblab-v0.49.14-d3dcebf-linux-arm.tar.gz"
+    sha256 "f1e67fcbf0ffd2397bd6564502f88aa0a4d75e19b31fabf901f52476f4b6a4f1"
   elsif OS.linux?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.13/c570a66/liblab-v0.49.13-c570a66-linux-x64.tar.gz"
-    sha256 "70f42d9a6ab36d330ffb0e05d0811bac88082afb458f51b79e2edf6275175312"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.14/d3dcebf/liblab-v0.49.14-d3dcebf-linux-x64.tar.gz"
+    sha256 "e6d788d048652fdaf780329e9b1230bc9669ad78ccc39f8c2b8fb08a9904a760"
   end
   depends_on "node"
   def install
