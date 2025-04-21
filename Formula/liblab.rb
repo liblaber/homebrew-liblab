d98d20bd61c62346be5474ffd0de78d1e09286f7
ATechAdventurer <cam.steeleis@gmail.com>
2025-04-21 12:21:04 -0500

    Test commit to GH

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -4,17 +4,17 @@ class Liblab < Formula
   homepage "https://liblab.com"
   version "0.49.19"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.19/d7aef01/liblab-v0.49.19-d7aef01-darwin-x64.tar.gz"
-    sha256 "4e78f8dd5c6bb126daf8b6d02d95e6a1679ca9408b61c13c52658b5c7d620b67"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.19/7ba2dee/liblab-v0.49.19-7ba2dee-darwin-x64.tar.gz"
+    sha256 "4f7fc06101ebfdb0d25d225f960b38ae46462a1cb5e822464902fcaa3406f02f"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.19/d7aef01/liblab-v0.49.19-d7aef01-darwin-arm64.tar.gz"
-    sha256 "635be265f2e246d04a46385cd03b1e2f61c05abdde10648f60135e6a2078695d"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.19/7ba2dee/liblab-v0.49.19-7ba2dee-darwin-arm64.tar.gz"
+    sha256 "0c6033c6e898a4669d7820b03d84d6de03e3c42e0126f18ab33e7f0d426f4fca"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.19/d7aef01/liblab-v0.49.19-d7aef01-linux-arm.tar.gz"
-    sha256 "14e345bebbf8a550bfd47ed849cb91fb107e809167224e0cfa49ff8acbe284fd"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.19/7ba2dee/liblab-v0.49.19-7ba2dee-linux-arm.tar.gz"
+    sha256 "cf4af7ec89db61dbde6f5d620016661ea9473ed78e81b0e22f3ce0cc4f4b03b0"
   elsif OS.linux?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.19/d7aef01/liblab-v0.49.19-d7aef01-linux-x64.tar.gz"
-    sha256 "5c2fe37cd98e714ace0baed3057f733e3a97da3503578c9fb30886812a612f5c"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.19/7ba2dee/liblab-v0.49.19-7ba2dee-linux-x64.tar.gz"
+    sha256 "8e07f9dcccf13d0b52f8a090bee07b346ad71b5b29347d51f8bbb07d3fd981bf"
   end
   depends_on "node"
   def install
