305f02165d581d4e0e1e6e2c1a6456b4a078f360
ATechAdventurer <cam.steeleis@gmail.com>
2025-05-22 15:49:38 -0500

    Update liblab.rb to version 0.49.36

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -2,19 +2,19 @@ require "language/node"
 class Liblab < Formula
   desc "liblab CLI - A CLI for generating SDKs (and docs) from API specs"
   homepage "https://liblab.com"
-  version "0.49.33"
+  version "0.49.36"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.33/4394fc3/liblab-v0.49.33-4394fc3-darwin-x64.tar.gz"
-    sha256 "232508503094a0cbca10144d7fafbbad9b7d1474a0db4811f40bdaddc3c87241"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.36/0922130/liblab-v0.49.36-0922130-darwin-x64.tar.gz"
+    sha256 "fc3c0d69fdd66e381a42a2cbec7d16c6e7f919ccb561ccb279acf9a4d5334779"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.33/4394fc3/liblab-v0.49.33-4394fc3-darwin-arm64.tar.gz"
-    sha256 "10d1c8a477813fff7d1a31cef5e042fd62870853e44b15685d0e2b509d85fe3a"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.36/0922130/liblab-v0.49.36-0922130-darwin-arm64.tar.gz"
+    sha256 "2dba860abc425c32d87bb868a729c58f07d617c5fe104595b5d700a6d4925377"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.33/4394fc3/liblab-v0.49.33-4394fc3-linux-arm.tar.gz"
-    sha256 "73d8595d9529d030ef664700f65b377dc2f87b67fc9a13b173aaf4a49c8891a4"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.36/0922130/liblab-v0.49.36-0922130-linux-arm.tar.gz"
+    sha256 "b3a47262b4d3665747124144cba3d8b3f970b900d6577ade5a23ebd0ea5f98c3"
   elsif OS.linux?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.33/4394fc3/liblab-v0.49.33-4394fc3-linux-x64.tar.gz"
-    sha256 "3c6deec252143eecca27841092798bb9e1b95b8fb952d5c16802caf9559616ea"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.36/0922130/liblab-v0.49.36-0922130-linux-x64.tar.gz"
+    sha256 "dd40e14197d89e565b4a3db3c857488f5b7ce1a37c2af645c3bc5c8302de35ab"
   end
   depends_on "node"
   def install
