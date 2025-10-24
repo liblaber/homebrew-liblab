089f72de118ee312231bfa8eb5ee432dafbbfecd
V Steele <cam.steeleis@gmail.com>
2025-10-24 11:13:04 -0500

    Update liblab.rb to version 0.49.48

--- Formula/liblab.rb
+++ Formula/liblab.rb
@@ -2,19 +2,19 @@ require "language/node"
 class Liblab < Formula
   desc "liblab CLI - A CLI for generating SDKs (and docs) from API specs"
   homepage "https://liblab.com"
-  version "0.49.47"
+  version "0.49.48"
   if OS.mac? && Hardware::CPU.intel?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.47/f0c26f2/liblab-v0.49.47-f0c26f2-darwin-x64.tar.gz"
-    sha256 "87accaa40f74cf86578d973a353bb28cc8e5ae262aced043bae54c5800c22b1f"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.48/0e49961/liblab-v0.49.48-0e49961-darwin-x64.tar.gz"
+    sha256 "a010ece2079279a9cb4eeac6cf9b64aa77561d885b91379a1c6194108e5cd586"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.47/f0c26f2/liblab-v0.49.47-f0c26f2-darwin-arm64.tar.gz"
-    sha256 "6148e9f19189ab372ec3032954f26c9d484f485eb971ca1742c58593855d8b2e"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.48/0e49961/liblab-v0.49.48-0e49961-darwin-arm64.tar.gz"
+    sha256 "311025e8f94453e6659484a3438656864f9c50e6647647abc67fbe04d5891785"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.47/f0c26f2/liblab-v0.49.47-f0c26f2-linux-arm.tar.gz"
-    sha256 "6bb12635e036ec34b4d29b26ebd8ee61fdd08357205cfb746a6405fb4c01ab76"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.48/0e49961/liblab-v0.49.48-0e49961-linux-arm.tar.gz"
+    sha256 "670d2fb4ed0021d5456e60d6d5bc89e7b3420f238e6771616a5aa38fafed6e30"
   elsif OS.linux?
-    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.47/f0c26f2/liblab-v0.49.47-f0c26f2-linux-x64.tar.gz"
-    sha256 "ec415a47bddec9aabcec5e2e402ccfd4caf5003a45452a712a3efc948f5fd2c6"
+    url "https://dev-liblabl-cli-binaries.s3.amazonaws.com/versions/0.49.48/0e49961/liblab-v0.49.48-0e49961-linux-x64.tar.gz"
+    sha256 "a4fccbbae80689c6fc496219ee788923a02c1342fb8e2d3976614e87b64e81bd"
   end
   depends_on "node"
   def install
